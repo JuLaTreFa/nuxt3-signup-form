d4df50a3979e48ec59995e69864c7223c7dc5198
codeDragon491 <trefasjulia@gmail.com>
2025-05-12 09:47:18 +0200

    Import Provet Cloud Design System and install Typescript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
   ssr: false, // enable client-side only rendering
+  vite: {
+    vue: {
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes('-')
+        }
+      }
+    },
+  },
+  css: ['@provetcloud/css'],
 })
