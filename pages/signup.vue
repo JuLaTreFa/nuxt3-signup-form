9214aa8cf113bedd6cbdfc6719f985f8e247f7d9
codeDragon491 <trefasjulia@gmail.com>
2025-05-16 10:19:36 +0200

    Build success page, fix eslint errors

--- pages/signup.vue
+++ pages/signup.vue
@@ -9,9 +9,9 @@
     </Head>
     <provet-stack style="max-width: 340px; margin: var(--n-space-xl) auto; padding: 0 1rem;">
       <provet-card padding="l">
-          <h1 slot="header">
-            Sign up to Nord
-          </h1>
+        <h1 slot="header">
+          Sign up to Nord
+        </h1>
         <SignupForm @submitted="onSubmitted" />
       </provet-card>
       <provet-card class="n-align-center">
---
+++ plugins/nordicons.client.ts
@@ -0,0 +1,9 @@
+import nordicons from '@nordhealth/icons'
+
+export default defineNuxtPlugin(() => {
+  return {
+    provide: {
+      nordicons,
+    },
+  }
+})
